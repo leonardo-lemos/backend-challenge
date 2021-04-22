0ba5d3a058318753a366313084cc6d0ed5940f7e
Leonardo Lemos <leonardolemos@live.com>
2021-04-22 16:39:38 -0300

    Mudanças nos testes

--- src/test/java/dev/leonardolemos/BackendchallengeTest.java
+++ src/test/java/dev/leonardolemos/BackendchallengeTest.java
@@ -1,21 +1,21 @@
-//package dev.leonardolemos;
-//
-//import io.micronaut.runtime.EmbeddedApplication;
-//import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.Assertions;
-//
-//import javax.inject.Inject;
-//
-//@MicronautTest
-//class BackendchallengeTest {
-//
-//    @Inject
-//    EmbeddedApplication<?> application;
-//
-//    @Test
-//    void testItWorks() {
-//        Assertions.assertTrue(application.isRunning());
-//    }
-//
-//}
+package dev.leonardolemos;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class BackendchallengeTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
